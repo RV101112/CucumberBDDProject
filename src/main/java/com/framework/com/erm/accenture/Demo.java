5b775bbbc634516f1181a28126cc6a2dff4c1ab0
RV101112 <ravisrm.raj101@gmail.com>
2024-07-28 13:47:59 +0530

    1st commit

---
+++ src/main/java/com/framework/com/erm/accenture/Demo.java
@@ -0,0 +1,14 @@
+package com.framework.com.erm.accenture;
+
+import java.util.*;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing jenkin run");
+		Map map= new HashMap<>();
+
+	}
+
+}
